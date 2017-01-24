1491b0a1d8fd24992313d9135b6cb0c45c6be73b
Afreen Bano <afreen.bano@pb.com>
2017-01-24 16:36:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ cp -vr config/server_scripts/ server-dist/conf
 
 cd  server-dist
 zip -r dist.zip conf
-cp -v config/code-deploy/code-deploy-config/appspec.yml server-dist/
+cp -v ../config/code-deploy/code-deploy-config/appspec.yml server-dist/
 cd ..
 
 echo "Deploying stack-app-${version}.zip into ${awsBucketName} bucket of application ${awsAppName} of ${awsAppGroupName} group"
